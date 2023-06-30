71361e885d941925110446bddc1086d018d0b301
Tarantini Pereira <taranttini@hotmail>
2023-06-30 02:22:54 -0300

    chore: ajustar teste de banco

--- 02-criando-api-rest-com-node-js/test/example.spec.ts
+++ 02-criando-api-rest-com-node-js/test/example.spec.ts
@@ -1,6 +1,16 @@
 import { app } from "../src/app";
+import { execSync } from "node:child_process";
+//import { beforeEach } from "node:test"; NAO_IMPORTAR_DO_NODE
 import request from "supertest";
-import { afterAll, beforeAll, describe, expect, it, test } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  test,
+} from "vitest";
 
 describe("Transactions routes", () => {
   // antes de todos 1 vez
@@ -10,6 +20,11 @@ describe("Transactions routes", () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex -- migrate:rollback --all");
+    execSync("npm run knex -- migrate:latest ");
   });
 
   it("should be able to create a new transaction", async () => {
@@ -23,7 +38,7 @@ describe("Transactions routes", () => {
       })
       .expect(201);
 
-    console.log(response.get("Set-Cookie"));
+    //console.log(response.get("Set-Cookie"));
   });
 
   it("should be able to list all transactions", async () => {
@@ -50,6 +65,75 @@ describe("Transactions routes", () => {
       }),
     ]);
 
-    console.log(listTransactionResponse.body);
+    // console.log(listTransactionResponse.body);
+  });
+
+  ///////////////////////////////////////////
+  it("should be able to get a specific transactions", async () => {
+    ///////////////////////////////////////////
+    // fazer a chamada http e criar a transação
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "new transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    const transactionId = listTransactionsResponse.body.transaction[0].id;
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set("Cookie", cookies)
+      .expect(200);
+
+    //expect(1).toEqual(1);
+
+    //console.log(getTransactionResponse.body.transaction);
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: "new transaction",
+        amount: 5000,
+      }),
+    );
+  });
+
+  it("should be able to get the summary", async () => {
+    // fazer a chamada http e criar a transação
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "credit transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    await request(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "debit transaction",
+        amount: 2000,
+        type: "debit",
+      });
+
+    const summaryResponse = await request(app.server)
+      .get("/transactions/summary")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    });
   });
 });
