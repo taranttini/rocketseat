d49b4634439a64fb5218b60216b1a07ab4f5ece1
Tarantini Pereira <taranttini@hotmail>
2023-07-02 13:47:03 -0300

    chore: ready

--- 02-criando-api-rest-com-node-js/src/server.ts
+++ 02-criando-api-rest-com-node-js/src/server.ts
@@ -1,4 +1,8 @@
 import { app } from "./app";
 import { env } from "./env";
 
+app.get("/ready", async (request, reply) => {
+  reply.send("ok");
+});
+
 app.listen({ port: env.PORT }).then(() => console.log("http server running!"));
