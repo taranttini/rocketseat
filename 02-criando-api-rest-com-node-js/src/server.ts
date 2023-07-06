ff6bdc96408524271f31acf5282df5971ef492c9
Tarantini Pereira <taranttini@hotmail>
2023-07-05 22:28:15 -0300

    test

--- 02-criando-api-rest-com-node-js/src/server.ts
+++ 02-criando-api-rest-com-node-js/src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
 //import { env } from "./env";
 
-const port = process.env.PORT || 3001; // env.PORT
+const port = process.env.PORT || "3001"; // env.PORT
 
-app.listen({ port }).then(() => console.log(`http server running! ${port}`));
+app
+  .listen({ port: parseInt(port) })
+  .then(() => console.log(`http server running! ${port}`));
