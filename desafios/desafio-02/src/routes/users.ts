45f093e04838ab747b0928c70222aeeed405c398
Tarantini Pereira <taranttini@hotmail>
2023-07-12 02:29:39 -0300

    feat: gerando hash de senha

--- desafios/desafio-02/src/routes/users.ts
+++ desafios/desafio-02/src/routes/users.ts
@@ -1,8 +1,20 @@
 import { knex } from "../database";
 import { checkSessionIdExists } from "../middelwares/check-session-id-exists";
+import * as bcrypt from "bcrypt";
 import { FastifyInstance } from "fastify";
 import crypto from "node:crypto";
-import { z } from "zod";
+import { object, z } from "zod";
+
+const Encrypt = {
+  cryptPassword: (password: string) =>
+    bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) => hash),
+
+  comparePassword: (password: string, hashPassword: string) =>
+    bcrypt.compare(password, hashPassword).then((resp) => resp),
+};
 
 export async function userRoutes(app: FastifyInstance) {
   app.addHook("preHandler", async (request) => {
@@ -35,11 +47,13 @@ export async function userRoutes(app: FastifyInstance) {
       maxAge: seven_days,
     });
 
+    const encryptedPassword = await Encrypt.cryptPassword(password);
+
     await knex("users").insert({
       id: crypto.randomUUID(),
       session_id: sessionId,
       username,
-      password,
+      password: encryptedPassword,
     });
 
     return reply.status(201).send();
@@ -52,18 +66,23 @@ export async function userRoutes(app: FastifyInstance) {
     });
 
     const { username, password } = createUserBodySchema.parse(request.body);
-    console.log("a", username, password);
 
     const sessionId = crypto.randomUUID();
 
     const seven_days = 1000 * 60 * 60 * 24 * 7;
 
     const user = await knex("users")
-      .select("id", "username")
-      .where({ username, password })
+      .select("id", "username", "password")
+      .where({ username })
       .first();
 
     if (!user) {
+      return reply.status(401).send({ error: "login fail" });
+    }
+
+    const valid = await Encrypt.comparePassword(password, user.password);
+
+    if (!valid) {
       return reply.status(401).send({ error: "login fail" });
     }
 
@@ -76,7 +95,7 @@ export async function userRoutes(app: FastifyInstance) {
       session_id: sessionId,
     });
 
-    return { user };
+    return { user: { id: user?.id, name: user?.username } };
   });
 
   app.get("/", { preHandler: [checkSessionIdExists] }, async (request) => {
@@ -89,4 +108,56 @@ export async function userRoutes(app: FastifyInstance) {
 
     return { user };
   });
+
+  app.get(
+    "/summary",
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const sessionId = request.cookies.sessionId;
+
+      const user = await knex("users")
+        .select("id")
+        .where({ session_id: sessionId })
+        .first();
+
+      if (!user) {
+        return reply
+          .status(401)
+          .send({ error: "user not found or not logged" });
+      }
+
+      const diets = await knex("diets")
+        .select("is_diet_valid")
+        .where({ user_id: user.id });
+
+      let actualSequence = 0;
+      let bestSequence = 0;
+
+      for (const diet of diets) {
+        if (diet.is_diet_valid) {
+          actualSequence++;
+          continue;
+        }
+
+        if (actualSequence > bestSequence) {
+          bestSequence = actualSequence;
+        }
+        actualSequence = 0;
+      }
+
+      //console.log(diets.map((q) => q.is_diet_valid));
+      const totalDietsRegistered = diets.length;
+      const totalDietsValid = diets.filter((q) => q.is_diet_valid).length;
+      const totalDietsNotValid = diets.filter((q) => !q.is_diet_valid).length;
+      //const totalDietsNotValid = totalDietsRegistered - totalDietsValid;
+
+      const sumary = {
+        totalDiets: totalDietsRegistered,
+        totalDietsValid: totalDietsValid,
+        totalDietsNotValid: totalDietsNotValid,
+        bestSequence: bestSequence,
+      };
+      return { sumary };
+    },
+  );
 }
