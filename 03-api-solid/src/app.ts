a2f0098c43c04223d0a0090922e52c5768edffad
Tarantini Pereira <taranttini@hotmail>
2023-07-23 00:13:15 -0300

    feat: erro global

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,6 +1,25 @@
+import { env } from "./env";
 import { appRoutes } from "./http/routes";
 import fastify from "fastify";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Validation error.",
+      issues: error.format(),
+    });
+  }
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+  return reply.status(500).send({
+    message: "internal server error.",
+  });
+});
