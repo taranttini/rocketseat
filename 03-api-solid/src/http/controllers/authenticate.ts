a2f38584983fedeea18defd5a588994604df759a
Tarantini Pereira <taranttini@hotmail>
2023-07-23 16:31:54 -0300

    feat: iniciar autenticacao

---
+++ 03-api-solid/src/http/controllers/authenticate.ts
@@ -0,0 +1,32 @@
+import { InvalidCreditialsError } from "errors/invalid-credentials-error";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { PrismaUsersRepository } from "repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "use-cases/authenticate";
+import { z } from "zod";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+
+    await authenticateUseCase.execute({ email, password });
+  } catch (err) {
+    if (err instanceof InvalidCreditialsError) {
+      return reply.status(400).send({ message: err.message });
+    }
+
+    throw err;
+  }
+
+  return reply.status(200).send();
+}
